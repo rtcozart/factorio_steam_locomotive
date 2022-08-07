47389e0808cf9887e3f3ec4145a156d7fdbef687
Tyler Cozart <rtcozart@gmail.com>
2022-08-06 20:09:52 -0500

    Update graphics

--- steam_locomotive.lua
+++ steam_locomotive.lua
@@ -7,57 +7,112 @@ end
 --TODO will probably create full prototype instead but this will do for now
 local steamLocomotive = table.deepcopy(data.raw["locomotive"]["locomotive"])
 steamLocomotive.name = "steam-locomotive"
-steamLocomotive.animation = {
-    layers = {
+steamLocomotive.steam_wheels = {
+    direction_count = 128,
+    frame_count = 8,
+    line_length = 8,
+    height = 512,
+    scale = 0.48,
+    shift = util.by_pixel(0, -18),
+    stripes = {
         {
-            direction_count = 64,
-            frame_count = 8,
-            line_length = 8,
-            height = 512,
-            stripes = {
-                {
-                    filename = sprite("steam-locomotive-1.png"),
-                    height_in_frames = 8,
-                    width_in_frames = 8
-                },{
-                    filename = sprite("steam-locomotive-2.png"),
-                    height_in_frames = 8,
-                    width_in_frames = 8
-                },{
-                    filename = sprite("steam-locomotive-3.png"),
-                    height_in_frames = 8,
-                    width_in_frames = 8
-                },{
-                    filename = sprite("steam-locomotive-4.png"),
-                    height_in_frames = 8,
-                    width_in_frames = 8
-                },{
-                    filename = sprite("steam-locomotive-5.png"),
-                    height_in_frames = 8,
-                    width_in_frames = 8
-                },{
-                    filename = sprite("steam-locomotive-6.png"),
-                    height_in_frames = 8,
-                    width_in_frames = 8
-                },{
-                    filename = sprite("steam-locomotive-7.png"),
-                    height_in_frames = 8,
-                    width_in_frames = 8
-                },{
-                    filename = sprite("steam-locomotive-8.png"),
-                    height_in_frames = 8,
-                    width_in_frames = 8
-                },
-            }
+            filename = sprite("steam-locomotive/wheels/sheet_0.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
         },{
-            direction_count = 64,
-            filename = sprite("steam-locomotive-shadow.png"),
-            draw_as_shadow = true,
-            line_length = 8,
-            lines_per_file = 8,
+            filename = sprite("steam-locomotive/wheels/sheet_1.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_2.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_3.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_4.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_5.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_6.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_7.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_8.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_9.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_10.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_11.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_12.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_13.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_14.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
+        },{
+            filename = sprite("steam-locomotive/wheels/sheet_15.gif"),
+            height_in_frames = 8,
+            width_in_frames = 8
         }
     }
 }
+
+steamLocomotive.pictures = {
+    layers = {
+        {
+            direction_count = 128,
+            line_length = 8,
+            lines_per_file = 8,
+            width = 512,
+            height = 512,
+            filenames = {
+                sprite("steam-locomotive/body/sheet_0.gif"),
+                sprite("steam-locomotive/body/sheet_1.gif")
+            },
+            scale = 0.48,
+            shift = util.by_pixel(0, -18)
+        }--,
+        --{
+        --    direction_count = 128,
+        --    filename = sprite("steam-locomotive/shadow.png"),
+        --    draw_as_shadow = true,
+        --    line_length = 8,
+        --    lines_per_file = 8,
+        --    height = 512,
+        --    width = 512,
+        --    scale = 0.6,
+        --    shift = util.by_pixel(0, 5)
+        --}
+    }
+}
+
+steamLocomotive.wheels = nil
 
 local recipe = {
     type = "recipe",
@@ -81,7 +136,7 @@ local recipe = {
 local item = {
     type = "item",
     name = "steam-locomotive-item",
-    icon = sprite("steam-locomotive-64x64.png"),
+    icon = sprite("steam-locomotive/64x64.png"),
     icon_size = 64,
     --subgroup = "logistics",
     --order = "z",
