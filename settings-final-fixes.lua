736c02e1b8487326d7370f14be3dbe439d9af3ab
Tyler Cozart <rtcozart@gmail.com>
2022-08-09 14:24:13 -0500

    Add honk compatibility

---
+++ settings-final-fixes.lua
@@ -0,0 +1,6 @@
+if mods["Honk"] then
+    local honk_group = data.raw["string-setting"]["honk-sound-locos-steam"].default_value
+    if not string.find(honk_group, "rtc[:]steam[-]locomotive") then
+        data.raw["string-setting"]["honk-sound-locos-steam"].default_value = honk_group..",rtc:steam_locomotive"
+    end
+end
